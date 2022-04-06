ff54d079b0acdfcde2cb40b3b2e541c2aec0e593
kyojunichi <junjundesuu@yahoo.co.jp>
2022-04-06 20:36:48 +0900

    Start project

---
+++ src/main/java/com/oinara/OinaraApplication.java
@@ -0,0 +1,13 @@
+package com.oinara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OinaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OinaraApplication.class, args);
+	}
+
+}
