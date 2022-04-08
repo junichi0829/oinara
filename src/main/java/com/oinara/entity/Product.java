5520448d868b802f5afd9398fa92c025415baae1
kyojunichi <junjundesuu@yahoo.co.jp>
2022-04-08 17:22:32 +0900

    Mapping Product class entity

--- src/main/java/com/oinara/entity/Product.java
+++ src/main/java/com/oinara/entity/Product.java
@@ -5,20 +5,32 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "product")
 @Getter
 @Setter
 @ToString
 public class Product {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int product_id; //상품id
 
+    @Column(name = "product_name", nullable = false, length = 50)
     private String product_name; //상품명
 
+    @Column(name = "price", nullable = false)
     private int price; //상품가격
 
+    @Lob
+    @Column(name = "description", nullable = false)
     private String description; //상품 상세 설명
 
+    @Enumerated(EnumType.STRING)
     private ProductSellStatus productSellStatus; //상품 판매 상태
 
     private LocalDateTime regTime; //등록시간
