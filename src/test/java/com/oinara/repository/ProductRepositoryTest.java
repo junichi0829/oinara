398e53ff97921e12df3012a3e608edab0ef7dd87
kyojunichi <junjundesuu@yahoo.co.jp>
2022-04-08 20:38:33 +0900

    Add Spring DATA JPA Querydsl & Testing

--- src/test/java/com/oinara/repository/ProductRepositoryTest.java
+++ src/test/java/com/oinara/repository/ProductRepositoryTest.java
@@ -2,12 +2,22 @@ package com.oinara.repository;
 
 import com.oinara.constant.ProductSellStatus;
 import com.oinara.entity.Product;
+import com.oinara.entity.QProduct;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sound.sampled.Port;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,6 +27,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ProductRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Autowired
     ProductRepository productRepository;
@@ -104,6 +117,76 @@ class ProductRepositoryTest {
         List<Product> productList = productRepository.findByDescriptionByNative("테스트 상품 상세 설명");
         for (Product product : productList) {
             System.out.println(product.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createProductList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QProduct qProduct = QProduct.product;
+        JPAQuery<Product> query = queryFactory.selectFrom(qProduct)
+                .where(qProduct.productSellStatus.eq(ProductSellStatus.SELL))
+                .where(qProduct.description.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qProduct.price.desc());
+
+        List<Product> productList = query.fetch();
+
+        for (Product product : productList) {
+            System.out.println(product.toString());
+        }
+    }
+
+    public void createProductList2() {
+        for (int i = 1; i <= 5; i++) {
+            Product product = new Product();
+            product.setProductName("테스트 상품" + i);
+            product.setPrice(10000 + i);
+            product.setDescription("테스트 상품 상세 설명" + i);
+            product.setProductSellStatus(ProductSellStatus.SELL);
+            product.setRegTime(LocalDateTime.now());
+            product.setUpdateTime(LocalDateTime.now());
+            productRepository.save(product);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+            Product product = new Product();
+            product.setProductName("테스트 상품" + i);
+            product.setPrice(10000 + i);
+            product.setDescription("테스트 상품 상세 설명" + i);
+            product.setProductSellStatus(ProductSellStatus.SOLD_OUT);
+            product.setRegTime(LocalDateTime.now());
+            product.setUpdateTime(LocalDateTime.now());
+            productRepository.save(product);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트2")
+    public void queryDslTest2() {
+        this.createProductList2();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QProduct product = QProduct.product;
+        String description = "테스트 상품 상세 설명";
+        int price = 10003;
+        String productSellStat = "SELL";
+
+        booleanBuilder.and(product.description.like("%" + description + "%"));
+        booleanBuilder.and(product.price.gt(price));
+
+        if (StringUtils.equals(productSellStat, ProductSellStatus.SELL)) {
+            booleanBuilder.and(product.productSellStatus.eq(ProductSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Product> productPagingResult = productRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + productPagingResult.getTotalElements());
+
+        List<Product> resultProductList = productPagingResult.getContent();
+        for (Product resultProduct : resultProductList) {
+            System.out.println(resultProduct.toString());
         }
     }
 }
