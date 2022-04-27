6687c6dfbe0df206dd6c69a957ac33475feb71a9
kyojunichi <junjundesuu@yahoo.co.jp>
2022-04-27 21:00:54 +0900

    Modify UserController

--- src/main/java/com/oinara/controller/UserController.java
+++ src/main/java/com/oinara/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.oinara.controller;
 
 import com.oinara.dto.UserFormDto;
+import com.oinara.entity.User;
 import com.oinara.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/users")
@@ -14,10 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String userForm(Model model) {
         model.addAttribute("userFormDto", new UserFormDto());
         return "user/userForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String userForm(UserFormDto userFormDto) {
+        User user = User.createUser(userFormDto, passwordEncoder);
+        userService.saveUser(user);
+
+        return "redirect:/";
     }
 }
