826659ebdcb8aebb5dd951716bc43c2826adf331
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-04 03:08:16 +0900

    Add front things in project and testing Thymeleaf

---
+++ src/main/java/com/oinara/controller/ThymeleafExController.java
@@ -0,0 +1,29 @@
+package com.oinara.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "타임리프 예제입니다");
+        return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07() {
+        return "thymeleafEx/thymeleafEx07";
+    }
+}
