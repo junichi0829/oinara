d5d610d824e23bf4ec6a27ade06d222e3e1ca33e
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-05 23:39:40 +0900

    Modify UserService

--- src/main/java/com/oinara/service/UserService.java
+++ src/main/java/com/oinara/service/UserService.java
@@ -3,13 +3,16 @@ package com.oinara.service;
 import com.oinara.entity.User;
 import com.oinara.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -23,5 +26,20 @@ public class UserService {
         if (findUser != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
+        User user = userRepository.findByAccount(account);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(account);
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getAccount())
+                .password(user.getPassword())
+                .roles(user.getRole().toString())
+                .build();
     }
 }
