d8daadf60378308a1f75238db7a2fb054a89d039
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-13 23:56:15 +0900

    Add OrderRepository

---
+++ src/main/java/com/oinara/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.oinara.repository;
+
+import com.oinara.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
