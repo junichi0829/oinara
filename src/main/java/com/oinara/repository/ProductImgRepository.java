0d2ab78e48c24d5731957d4063f8c7920e4b2f58
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-18 12:48:28 +0900

    Add FileService.java & Add ProductImgRepository.java

---
+++ src/main/java/com/oinara/repository/ProductImgRepository.java
@@ -0,0 +1,8 @@
+package com.oinara.repository;
+
+import com.oinara.entity.ProductImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductImgRepository extends JpaRepository<ProductImg, Long> {
+
+}
