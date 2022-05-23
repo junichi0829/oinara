5b62591bed334a357b823beccc3dda869152d97b
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-23 12:49:33 +0900

    Add ProductRepositoryCustom.java

---
+++ src/main/java/com/oinara/repository/ProductRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.oinara.repository;
+
+import com.oinara.dto.ProductSearchDto;
+import com.oinara.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductRepositoryCustom {
+
+    Page<Product> getAdminProductPage(ProductSearchDto productSearchDto, Pageable pageable);
+
+}
