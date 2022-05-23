55b1038e5019c888dec22cb74a88d35c096098fc
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-23 12:47:10 +0900

    Add ProductSearchDto.java

---
+++ src/main/java/com/oinara/dto/ProductSearchDto.java
@@ -0,0 +1,18 @@
+package com.oinara.dto;
+
+import com.oinara.constant.ProductSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProductSearchDto {
+
+    private String searchDataType;
+
+    private ProductSellStatus productSellStatus;
+
+    private String searchBy;
+
+    private String searchQuery = "";
+
+}
