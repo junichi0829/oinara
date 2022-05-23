ec3082763c3bac8e288ed6fd22452ffbc0a9c5b3
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-23 13:27:11 +0900

    Modify ProductController.java

--- src/main/java/com/oinara/controller/ProductController.java
+++ src/main/java/com/oinara/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.oinara.controller;
 
 import com.oinara.dto.ProductFormDto;
+import com.oinara.dto.ProductSearchDto;
 import com.oinara.entity.Product;
 import com.oinara.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,11 +37,11 @@ public class ProductController {
     @PostMapping(value = "/admin/product/new")
     public String productNew(@Valid ProductFormDto productFormDto, BindingResult bindingResult, Model model, @RequestParam("productImgFile") List<MultipartFile> productImgFileList) {
 
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "product/productForm";
         }
 
-        if(productImgFileList.get(0).isEmpty() && productFormDto.getId() == null){
+        if (productImgFileList.get(0).isEmpty() && productFormDto.getId() == null) {
             model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수입니다.");
             return "product/productForm";
         }
@@ -85,5 +90,15 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = {"/admin/products", "/admin/products/{page}"})
+    public String productManage(ProductSearchDto productSearchDto, @PathVariable("page") Optional<Integer> page, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Product> products = productService.getAdminProductPage(productSearchDto, pageable);
+        model.addAttribute("products", products);
+        model.addAttribute("productSearchDto", productSearchDto);
+        model.addAttribute("maxPage", 5);
+        return "product/productMng";
     }
 }
