df4569f782fc0a57de837ab8922e7e763a76b12f
kyojunichi <junjundesuu@yahoo.co.jp>
2022-05-30 13:44:01 +0900

    Modify ProductLikeRepository

--- src/main/java/com/oinara/repository/ProductLikeRepository.java
+++ src/main/java/com/oinara/repository/ProductLikeRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
 
+    ProductLike findByUserId(Long userId);
+
 }
